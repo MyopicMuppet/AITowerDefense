4b9c69406bd933b20f47db9a5144d182810fb0f3
MyopicMuppet <47373110+MyopicMuppet@users.noreply.github.com>
2019-03-26 11:56:31 +1100

    class camera update

--- Assets/TowerDefense/Scripts/Cameras/CameraOrbit_old.cs
+++ Assets/TowerDefense/Scripts/Cameras/CameraOrbit_old.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class CameraOrbit : MonoBehaviour
 {
-
     public Transform target;
-    //Distance the camera is from world zero
+    // Distance the camera is from world zero
     public float distance = 10f;
-    //X and Y rotation speed
+    // X and Y rotation speed
     public float xSpeed = 120f, ySpeed = 120f;
-    //X and Y rotation limits
+    // X and Y rotation limits
     public float yMin = 15f, yMax = 80f;
-    //Store current x and y rotation
+    // Store current x and y rotation
     private float x, y;
 
     void Start()
@@ -22,29 +21,29 @@ public class CameraOrbit : MonoBehaviour
         y = euler.x;
     }
 
-
     // Update is called once per frame
     void LateUpdate()
     {
-        //If the left right mouse button is pressed
+        // If the right mouse button is pressed
         if (Input.GetMouseButton(1))
         {
-
-            // Get input X and Y offset
+            // Hide the cursor
+            Cursor.visible = false;
+            // Get input X and Y offsets
             float mouseX = Input.GetAxis("Mouse X");
             float mouseY = Input.GetAxis("Mouse Y");
-            //Offset rotation with mouse X and Y
+            // Offset rotation with mouse X and Y
             x += mouseX * xSpeed * Time.deltaTime;
             if (Input.GetKey(KeyCode.UpArrow))
             {
                 y -= mouseY * ySpeed * Time.deltaTime;
             }
-            y -= mouseY * ySpeed * Time.deltaTime;
-            //Clamp the Y between min and max limits
+            // Clamp the Y between min and max limits
             //y = Mathf.Clamp(y, yMin, yMax);
         }
-        //Update transform
+
+        // Update transform
         transform.rotation = Quaternion.Euler(y, x, 0);
-        transform.position = -transform.forward * distance;
+        transform.position = target.position - transform.forward * distance;
     }
 }
